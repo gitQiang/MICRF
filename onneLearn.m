560d6fbef003b65ab03fa44abc8bc9b8a45a64bf
Qiangshenlab <hq04405114@gmail.com>
2015-11-16 17:15:12 -0500

    max logistic 10 to edge weights

--- onneLearn.m
+++ onneLearn.m
@@ -64,11 +64,11 @@ for e = 1:nEdges
     x11 = min(0.5 * (nodePot(n1,1) + nodePot(n2,1)),0.9999999);
     x22 = min(0.5 * (nodePot(n1,2) + nodePot(n2,2)),0.9999999);
     
-    edgePot(1,1,e) =   max(1, 2/(1+exp( -(0.94/0.06)*(x11-0.5) ) ))*F(sube);
+    edgePot(1,1,e) =   max(1, 10/(1+exp(-(0.94/0.06)*(x11-0.5)))-4)*F(sube);
     edgePot(1,2,e) =   F(sube);
     edgePot(2,1,e) =   F(sube);
     edgePot(2,2,e) =   max(1,2*x22)*F(sube);
 end
 
 clear adj
-save iRef_logist_sum.mat 
+save iRef_logist_sum_10.mat 
