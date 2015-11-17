b9dbcadda4ad0b64e53c506dc2d2d494da9b3832
Qiangshenlab <hq04405114@gmail.com>
2015-11-16 20:10:51 -0500

    posterior and prior to approximate edge weights v2

--- onneLearn.m
+++ onneLearn.m
@@ -67,11 +67,11 @@ for e = 1:nEdges
     x11 = min(0.5 * (nodePot(n1,1) + nodePot(n2,1)),0.9999999);
     x22 = min(0.5 * (nodePot(n1,2) + nodePot(n2,2)),0.9999999);
     
-    edgePot(1,1,e) =   (1+x11)*F(sube);
+    edgePot(1,1,e) =   (1+2*x11)*F(sube);
     edgePot(1,2,e) =   F(sube);
     edgePot(2,1,e) =   F(sube);
-    edgePot(2,2,e) =   (1+x22)*F(sube);
+    edgePot(2,2,e) =   (1+2*x22)*F(sube);
 end
 
 clear adj
-save iRef_logist_sum_10.mat 
+save iRef_sim_2.mat 
