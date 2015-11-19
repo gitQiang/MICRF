74de7291949a4d43fab8a8d25d0bd7234704c527
Qiangshenlab <hq04405114@gmail.com>
2015-11-19 11:45:02 -0500

    100 initials training on all data: faster

--- oneLearn.m
+++ oneLearn.m
@@ -1,5 +1,6 @@
-function [w,f,Y]=oneLearn(netfile,beness,nodef)
+function [w,f,Y]=oneLearn(netfile,beness,nodef,adjf)
 
+load(adjf);
 %% read the network edges and node
 [node1,node2,weights] = textread(netfile,'%s%s%f','delimiter','\t');
 genes = union(node1,node2);
@@ -11,23 +12,22 @@ maxState = max(nStates);
 edgeStruct = UGM_makeEdgeStruct(adj,nStates); 
 nEdges = edgeStruct.nEdges;
 
-%% Make the non-negative node and edge potentials
 % read TADA score
-[nodes, score1] = textread(nodef,'%s%f','delimiter','\t');
+fcon =  fopen(nodef,'r');
+C = textscan(fcon,'%s%f','delimiter','\t');
+nodes= C{1};
+score1=C{2};
+fclose(fcon);
 node_s = intersect(nodes,genes);
 % read edge betweenness
 [enode1,enode2,be] = textread(beness,'%s -- %s\t%f');
-%be = be /max(be);
-n_e = length(be);
-F = zeros(1,n_e);
-for i=1:n_e
-    F(i) = sum(be <= be(i))/n_e;
-end
+F=tiedrank(be)/length(be);
 
+edgesb1 = strcat(enode1,'_',enode2);
+edgesb2 = strcat(enode2,'_',enode1);
 
-%% make pseudo potential functions
+% make pseudo potential functions
 % 1 risk state, 2 non-risk state
-
 Pri=zeros(2,1);
 Pri(1)=0.06;
 Pri(2)=0.94;
@@ -35,41 +35,24 @@ Pri(2)=0.94;
 nodePot = ones(nNodes,maxState); %!!!!!!!!! zeros and ones for node information
 nodePot(:,1) = Pri(1)*Pri(2);
 nodePot(:,2) = Pri(2)*Pri(1);
-
-for n = 1:length(node_s)
-     [~,j] = ismember(node_s(n),genes);
-     [~,i] = ismember(node_s(n),nodes);
-        
-     nodePot(j,1) = score1(i)*Pri(2);
-     nodePot(j,2) = (1 - score1(i))*Pri(1);
-end
-
+[~,j] = ismember(node_s,genes);
+[~,i] = ismember(node_s,nodes);        
+nodePot(j,1) = score1(i)*Pri(2);
+nodePot(j,2) = (1 - score1(i))*Pri(1);
 
 % Make (non-negative) potential of each edge taking each state combination
 edgePot = ones(maxState,maxState,edgeStruct.nEdges);
-for e = 1:nEdges
-	n1 = edgeStruct.edgeEnds(e,1);
-	n2 = edgeStruct.edgeEnds(e,2);
-    edgePot(:,:,e) = ones(nStates(n1),nStates(n2));
-    
-    tmp1 = ismember(enode1,genes(n1));
-    tmp2 = ismember(enode2,genes(n2));
-    tmp = tmp1 + tmp2;
-    
-    if max(tmp) < 2 
-        tmp1 = ismember(enode1,genes(n2));
-        tmp2 = ismember(enode2,genes(n1));
-        tmp = tmp1 + tmp2;
-    end
-    sube = find(tmp==2);
-    
-    %% important edge potential definition
-    
-    edgePot(1,1,e) =   (1+sqrt(nodePot(n1,1) * nodePot(n2,1)))*F(sube);
-    edgePot(1,2,e) =   F(sube);
-    edgePot(2,1,e) =   F(sube);
-    edgePot(2,2,e) =   (1+sqrt(nodePot(n1,2) * nodePot(n2,2)))*F(sube);
-end
+n1 = edgeStruct.edgeEnds(:,1);
+n2 = edgeStruct.edgeEnds(:,2);
+edges = strcat(cellstr(genes(n1)),'_',cellstr(genes(n2)));
+[~,ind1] = ismember(edges,edgesb1);
+[~,ind2] = ismember(edges,edgesb2);
+sube = ind1 + ind2;
+%% important edge potential definition
+edgePot(1,1,:) =   (1+sqrt(nodePot(n1,1) .* nodePot(n2,1))).*F(sube);
+edgePot(1,2,:) =   F(sube);
+edgePot(2,1,:) =   F(sube);
+edgePot(2,2,:) =   (1+sqrt(nodePot(n1,2) .* nodePot(n2,2))).*F(sube);
 
 clear adj
 save BrainCor_sim.mat 
